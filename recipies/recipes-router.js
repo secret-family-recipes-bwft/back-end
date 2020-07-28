0ff15bab35181c7b85ffeff2cd9f3912d7b72f08
Marta Krawczyk <martaalicja113@gmail.com>
2020-07-28 11:50:05 -0400

    new dependencies, implement req.queries in recipe-router

--- recipies/recipes-router.js
+++ recipies/recipes-router.js
@@ -3,54 +3,71 @@ const router = require('express').Router()
 const RecipeModel = require('./recipes-model')
 
 //````````GET``````````
-//get recipes
+// get recipes
 router.get('/', (req, res) => {
-    RecipeModel.findRecipes()
-    .then( recipes => {
-        res.json(recipes)
-    })
-    .catch( err => {
-        res.status(500).json({error: err.message})
-    })
+    const { category } = req.query
+    if(req.query.category){ //localhost:5000/api/recipes?category=dinner
+        RecipeModel.findRecipesBy({ category })
+        .then(recipes => {
+            if (!recipes.length) {
+                res.status(404).json({ message: 'no recipes with that category' })
+            } else {
+                res.json(recipes)
+            }
+
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
+    } else { //localhost:5000/api/recipes
+        RecipeModel.findRecipes()
+        .then(recipes => {
+            res.json(recipes)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
+    }
+  
 })
 
 //get recipe by id
-router.get('/:id',validateRecipeId, (req, res) => {
+router.get('/:id', validateRecipeId, (req, res) => {
     RecipeModel.findRecipeById(req.params.id)
-    .then( recipe => {
-        res.json(recipe)
-    })
-    .catch( err => {
-        res.status(500).json({error: err.message})
-    })
+        .then(recipe => {
+            res.json(recipe)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
 })
 
 //get recipes by category
-router.get('/category/:category', (req, res) => {
-    const category = req.params.category  
-    RecipeModel.findRecipesBy({ category })
-    .then( recipes => {
-        if(!recipes.length){
-            res.status(404).json({message: 'no recipes with that category'})
-        } else {
-            res.json(recipes)
-        }
-       
-    })
-    .catch( err => {
-        res.status(500).json({error: err.message})
-    })
-})
+// router.get('/', (req, res) => { //--> combine into the base GET 
+//     const { category } = req.query
+//     RecipeModel.findRecipesBy({ category })
+//         .then(recipes => {
+//             if (!recipes.length) {
+//                 res.status(404).json({ message: 'no recipes with that category' })
+//             } else {
+//                 res.json(recipes)
+//             }
+
+//         })
+//         .catch(err => {
+//             res.status(500).json({ error: err.message })
+//         })
+// })
 
 //get most recent 3 recipes
 router.get('/recent/:limit', (req, res) => {
     RecipeModel.findRecipes().orderBy('id', 'desc').limit(req.params.limit)
-    .then( recipes => {
-        res.json(recipes)
-    })
-    .catch( err => {
-        res.status(500).json({error: err.message})
-    })
+        .then(recipes => {
+            res.json(recipes)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
 })
 
 //``````POST````````
@@ -58,29 +75,29 @@ router.get('/recent/:limit', (req, res) => {
 
 
 //``````PUT``````````
-router.put('/:id',validateRecipeId, (req, res) => {
-    if(!Object.keys(req.body).length){
-        res.status(400).json({message: 'nothing to update was provided...'})
+router.put('/:id', validateRecipeId, (req, res) => {
+    if (!Object.keys(req.body).length) {
+        res.status(400).json({ message: 'nothing to update was provided...' })
     }
 
     RecipeModel.updateRecipe(req.body, req.params.id)
-    .then( updatedRecipe => {
-        res.json({success: 'updated!', updatedRecipe})
-    })
-    .catch( err => {
-        res.status(500).json({error: err.message})
-    })
+        .then(updatedRecipe => {
+            res.json({ success: 'updated!', updatedRecipe })
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
 })
 
 //``````DELETE```````
-router.delete('/:id',validateRecipeId, (req, res) => {
+router.delete('/:id', validateRecipeId, (req, res) => {
     RecipeModel.removeRecipe(req.params.id)
-    .then( deletedRecipe => {
-        res.json({success: 'deleted!', deletedRecipe})
-    })
-    .catch( err => {
-        res.status(500).json({error: err.message})
-    })
+        .then(deletedRecipe => {
+            res.json({ success: 'deleted!', deletedRecipe })
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
 })
 
 //````````custom Middleware``````
